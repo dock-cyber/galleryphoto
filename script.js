5194312702fec357d0c934c8c4b5cacc0c0bd47a
dock-cyber <51847952+dock-cyber@users.noreply.github.com>
2019-06-20 17:27:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const gambarJumbo = document.querySelector('.jumbo');
-const thumbnail = document.querySelector('.thumbnail');
+const container = document.querySelector('.container');
+const thumbs = document.querySelectorAll('.thum');
 
-thumbnail.addEventListener('click', function (e) {
+container.addEventListener('click', function (e) {
     if (e.target.className == 'thum') {
         const eSrc = e.target.getAttribute('src');
         gambarJumbo.setAttribute('src', eSrc);
+        gambarJumbo.classList.add('fade');
+        setTimeout(function () {
+            gambarJumbo.classList.remove('fade');
+
+        }, 100);
+
+        thumbs.forEach(function (thum) {
+            thum.className = 'thum';
+        });
+
+        e.target.classList.add('active');
+
     }
 });
